4b7f002d8c56a6806cdc972cde2d27369398686d
KavithaMuthukumaran <mp.kavithaa@gmail.com>
2023-07-28 18:31:54 +0530

    commit

--- LargestOfThreeNumbers.py
+++ LargestOfThreeNumbers.py
@@ -1,6 +1,10 @@
-a = 25
-b = 50
-c = 10
+# a = 25
+# b = 50
+# c = 10
+
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+c=int(input("Enter the thrid number:"))
 
 if a>b and a>c:
     largest = a
