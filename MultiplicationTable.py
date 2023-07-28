18c5fc5a233d2afedcaa187025188ae93a955fde
KavithaMuthukumaran <mp.kavithaa@gmail.com>
2023-07-28 21:09:27 +0530

    Multiplication table

---
+++ MultiplicationTable.py
@@ -0,0 +1,5 @@
+table_number = int(input("Enter the Multiplication table number:"))
+
+for i in range(1,11):
+    #print(f"\table_number} * {i} = {i*table_number}")
+    print(table_number,'X',i,'=',table_number*i)
